6899a68ce54fb0a021eb2dcce786452cd660d52d
Kim Wee <sim.kim.wee.012s@gmail.com>
2024-04-26 23:30:12 +0800

    Heston Model calibration updates (#13)
    * Updates:
    
    made changes to code design for calibration function

--- code_base/services/calibrators/model_calib/heston_model.py
+++ code_base/services/calibrators/model_calib/heston_model.py
@@ -25,19 +25,19 @@ class HestonModel(object):
     def __init__(self,
                  spot: float,
                  strike: float,
-                 initial_variance: float,
                  interest: float,
-                 kappa: float,
-                 theta: float,
-                 sigma: float,
-                 rho: float,
-                 lamda: float,
                  maturity: float,
+                 initial_variance: float = 0.0,
+                 kappa: float = 0.0,
+                 theta: float = 0.0,
+                 sigma: float = 0.0,
+                 rho: float = 0.0,
+                 lamda: float = 0.0,
                  ) -> None:
-        self._spot: float = spot
-        self._strike: float = strike
-        self._maturity: float = maturity
-        self._interest: float = interest
+        self._spot: [float] = spot
+        self._strike: [float] = strike
+        self._maturity: [float] = maturity
+        self._interest: [float] = interest
         self._theta: float = theta
         self._kappa: float = kappa
         self._sigma: float = sigma
@@ -45,58 +45,63 @@ class HestonModel(object):
         self._initial_variance: float = initial_variance
         self._lamda: float = lamda
 
-    def define_heston_features(self, phi):
+          
+    @staticmethod
+    def define_heston_features(phi: float, kappa: float, theta: float, sigma: float, rho: float, lamda: float, maturity: float, interest: float, initial_variance: float, spot: float):
         # constants
-        _a = self._kappa * self._theta
-        _b = self._kappa + self._lamda
+        _a = kappa * theta
+        _b = kappa + lamda
 
         # common terms w.r.t phi
-        rspi = self._rho * self._sigma * phi * 1j
+        rspi = rho * sigma * phi * 1j
 
         # define d parameter given phi and b
         _d = np.sqrt(
-            (self._rho * self._sigma * phi * 1j - _b) ** 2 + (phi * 1j + phi ** 2) * self._sigma ** 2)
+            (rho * sigma * phi * 1j - _b) ** 2 + (phi * 1j + phi ** 2) * sigma ** 2)
 
         # define g parameter given phi, b and d
         g = (_b - rspi + _d) / (_b - rspi - _d)
 
         # calculate characteristic function by components
-        exp1 = np.exp(self._interest * phi * 1j * self._maturity)
-        term2 = self._spot ** (phi * 1j) * ((1 - g * np.exp(_d * self._maturity)) / (1 - g)) ** (
-                -2 * _a / self._sigma ** 2)
+
+        exp1 = np.exp(interest * phi * 1j * maturity)
+        term2 = spot ** (phi * 1j) * ((1 - g * np.exp(_d * maturity)) / (1 - g)) ** (
+                -2 * _a / sigma ** 2)
         exp2 = np.exp(
-            _a * self._maturity * (_b - rspi + _d) / self._sigma ** 2 + self._initial_variance * (_b - rspi + _d) * (
-                    (1 - np.exp(_d * self._maturity)) / (1 - g * np.exp(_d * self._maturity))) / self._sigma ** 2)
+            _a * maturity * (_b - rspi + _d) / sigma ** 2 + initial_variance * (_b - rspi + _d) * (
+                    (1 - np.exp(_d * maturity)) / (1 - g * np.exp(_d * maturity))) / sigma ** 2)
 
         return exp1 * term2 * exp2
 
-    def integrand(self, phi):
-        numerator = np.exp(self._interest * self._maturity) * self.define_heston_features(
-            phi - 1j) - self._strike * self.define_heston_features(phi)
-        denominator = 1j * phi * self._strike ** (1j * phi)
+    @staticmethod
+    def integrand(phi, interest, maturity, strike, kappa, theta, sigma, rho, lamda, initial_variance, spot):
+        numerator = (np.exp(interest * maturity) *
+                     HestonModel.define_heston_features(phi - 1j, kappa, theta, sigma, rho, lamda, maturity, interest, initial_variance, spot) - strike *
+                     HestonModel.define_heston_features(phi, kappa, theta, sigma, rho, lamda, maturity, interest, initial_variance, spot))
+        denominator = 1j * phi * strike ** (1j * phi)
         return numerator / denominator
 
-    def price(self, **kwargs):
-        real_integral, err = np.real(quad(self.integrand, 0, 100))
-        return (self._spot - self._strike * np.exp(-self._interest * self._maturity)) / 2 + real_integral / np.pi
-
     @staticmethod
-    def calibrate(last_trade_price: [float], rate: [float], strike: [float], maturity: [float], price: [float],
-                  henston_pricer, squared_error, params, ):
-        x0 = [param["x0"] for key, param in params.items()]
+    def price(spot, strike, interest, maturity, *args, **kwargs):
+        real_integral, err = np.real(quad(HestonModel.integrand, 0, 100, args=args))
+        return (spot - strike * np.exp(-interest * maturity)) / 2 + real_integral / np.pi
+
+    def calibrate(self, squared_error, params):
+        x0 = np.array([param["x0"] for key, param in params.items()])
+
         heston_bounds = [param["lbub"] for key, param in params.items()]
 
         result = minimize(squared_error, x0, tol=1e-3, method='SLSQP', options={'maxiter': 1e4}, bounds=heston_bounds)
 
         return result
 
-    def squaredError(self, last_trade_price: [float], rate: [float], strike: [float], maturity: [float], price: [float]):
-        v0, kappa, theta, sigma, rho, lambd = [param for param in x]
+    def squared_error(self, x: np.ndarray, x0: np.ndarray, last_trade_price: [float], rate: [float], strike: [float], maturity: [float], price: [float]):
+        v0, kappa, theta, sigma, rho, lamda = [param for param in x]
 
         # Attempted to use scipy integrate quad module as constrained to single floats not arrays
-        err = np.sum([(P_i - self.price(last_trade_price, K_i, v0, kappa, theta, sigma, rho, lambd, tau_i,
-                                            r_i)) ** 2 / len(price)
-                      for P_i, K_i, tau_i, r_i in zip(price, strike, maturity, rate)])
+        err = np.sum([
+            (P_i - self.price(last_trade_price, K_i, v0, kappa, theta, sigma, rho, lamda, maturity, r_i)) ** 2 / len(price) for P_i, K_i, tau_i, r_i in zip(price, strike, maturity, rate)
+        ])
 
         # Zero penalty term - no good guesses for parameters
         # pen = 0
